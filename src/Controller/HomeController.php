519afc52d8bd599542c0ee5079d2c4d0ceb990c0
gregoweb <gregoweb@user.noreply.github.com>
2021-06-02 12:30:53 +0200

    feat generate a Name with phoneme array

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,18 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $phoneme= ['zu','zo','dor','bi','ko','zan','mi','mo','mor','do','ri','po','pi','ka','chu','ar','di','ta','nia'];
+        $phonemeMax = count($phoneme)-1;
+        $nbPhoneme = rand (2, 4);
+        $aleatName = '';
+        while ($nbPhoneme > 0) {
+            $nbPhoneme --;
+            $aleatName .= $phoneme[random_int (0, $phonemeMax)];
+        }
+        $aleatName=ucfirst($aleatName);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'aleat_name' => $aleatName,
         ]);
     }
 }
